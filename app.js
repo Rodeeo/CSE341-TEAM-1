1e9622dee07c16cc22a33ae36a9128f4cd319467
saraytin <smee.tindall@gmail.com>
2021-06-09 20:36:21 -0500

    useNewUrlParser option

--- app.js
+++ app.js
@@ -1,62 +1,59 @@
-const path = require('path');
+const path = require("path");
 
-const cors = require('cors')
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const session = require('express-session');
-const mongoConnect = require('./util/database').mongoConnect;
-const MongoDBStore = require('connect-mongodb-session')(session);
-
+const cors = require("cors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const mongoConnect = require("./util/database").mongoConnect;
+const MongoDBStore = require("connect-mongodb-session")(session);
 
 // const corsOptions = {
 //     origin: "https://tiempo-team1.herokuapp.com/",
 //     optionsSuccessStatus: 200
 //   };
 //   app.use(cors(corsOptions));
- 
-  const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-    family: 4
-  };
+
+const options = {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  family: 4,
+};
 
 const PORT = process.env.PORT || 5000; // So we can run on heroku || (OR) localhost:5000
-const MONGODB_URL  = 'mongodb+srv://Bob:Bob123@tiempo-team1.hrin5.mongodb.net/Tiempo-Team1';
+const MONGODB_URL = "mongodb+srv://Bob:Bob123@tiempo-team1.hrin5.mongodb.net/Tiempo-Team1";
 
-  const store = new MongoDBStore({
-    uri: MONGODB_URL,
-    collection: 'sessions'
-  });
+const store = new MongoDBStore({
+  uri: MONGODB_URL,
+  collection: "sessions",
+});
 
 const app = express();
-app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set("view engine", "ejs");
+app.set("views", "views");
 
-const timecardRoutes = require('./routes/timecard');
+const timecardRoutes = require("./routes/timecard");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
-    secret: 'my secret',
+    secret: "my secret",
     resave: false,
     saveUninitialized: false,
-    store: store
+    store: store,
   })
 );
 app.use(timecardRoutes);
 
-
 mongoose
-.connect(MONGODB_URL)
-.then(result => {
-  app.listen(PORT);
-})
-.catch(err => {
-  console.log(err);
-});
-
-
+  // Passing options to avoid using deprecated parser
+  .connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then((result) => {
+    app.listen(PORT);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

7ad6abb9fb2d26192064d506d4749686137e0e76
alainluthier <alainluthier@gmail.com>
2021-06-12 07:26:51 -0400

    entertime-totaltime

--- controllers/timecard.js
+++ controllers/timecard.js
@@ -1,8 +1,30 @@
-
-
+const ITEMS_PER_PAGE = 10;
 exports.getTime = (req, res, next) => {
-      res.render('index', {
-        pageTitle: 'Home',
-        path: '/',
-      });
-  };
+  res.render('index', {
+    pageTitle: 'Home',
+    path: '/',
+  });
+};
+exports.enterTime = (req, res, next) => {
+  res.render('time/entertime', {
+    pageTitle: 'Enter Time',
+    path: '/entertime',
+  });
+};
+exports.totalTime = (req, res, next) => {
+  const page = +req.query.page || 1;
+  var dataJson = require('../models/data.json');
+  let totalItems = dataJson.length;
+  let data=dataJson.slice((page-1)*ITEMS_PER_PAGE,((page-1)*ITEMS_PER_PAGE)+ITEMS_PER_PAGE);
+  res.render('time/totaltime', {
+    pageTitle: 'Total Time',
+    path: '/totaltime',
+    data: data,
+    currentPage: page,
+    hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+    hasPreviousPage: page > 1,
+    nextPage: page + 1,
+    previousPage: page - 1,
+    lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+  });
+};
--- routes/timecard.js
+++ routes/timecard.js
@@ -8,5 +8,6 @@ const timecardController = require('../controllers/timecard');
 const router = express.Router();
 
 router.get('/', timecardController.getTime);
-
+router.get('/enterTime', timecardController.enterTime);
+router.get('/totalTime', timecardController.totalTime);
 module.exports = router;

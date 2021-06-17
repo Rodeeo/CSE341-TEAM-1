513b53f7ba4a6de042ef032bfd3511bff35364f8
ruben-xe <rod17015@byui.edu>
2021-06-17 02:30:42 -0300

    auth with JWT

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,38 +1,103 @@
+const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 
-
-exports.getLogin = (req, res, next) => {
+exports.login = (req, res, next) => {
+    const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        const error = new Error('A user with this email could not be found.');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+    })
+    .then(isEqual => {
+      if (!isEqual) {
+        const error = new Error('Wrong password!');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign(
+        {
+          email: loadedUser.email,
+          userId: loadedUser._id.toString()
+        },
+        'supersecretsecret',
+        { expiresIn: '1h' }
+      );
+      res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 
 };
 
-exports.getSignup = (req, res, next) => {
-
+exports.signup = (req, res, next) => {
+    const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  const email = req.body.email;
+  const name = req.body.name;
+  const password = req.body.password;
+  bcrypt
+    .hash(password, 12)
+    .then(hashedPw => {
+      const user = new User({
+        email: email,
+        password: hashedPw,
+        name: name
+      });
+      return user.save();
+    })
+    .then(result => {
+      res.status(201).json({ message: 'User created!', userId: result._id });
+    })
+    .catch(err => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
 
-exports.postLogin = (req, res, next) => {
+// exports.postLogin = (req, res, next) => {
 
-};
+// };
 
-exports.postSignup = (req, res, next) => {
+// exports.postSignup = (req, res, next) => {
 
-};
+// };
 
-exports.postLogout = (req, res, next) => {
-    res.redirect('/');
-};
+// exports.postLogout = (req, res, next) => {
+//     res.redirect('/');
+// };
 
-exports.getReset = (req, res, next) => {
+// exports.getReset = (req, res, next) => {
 
-};
+// };
 
-exports.postReset = (req, res, next) => {
+// exports.postReset = (req, res, next) => {
 
-};
+// };
 
-exports.getNewPassword = (req, res, next) => {
+// exports.getNewPassword = (req, res, next) => {
 
-};
+// };
 
-exports.postNewPassword = (req, res, next) => {
-    res.redirect('/login');
-};
+// exports.postNewPassword = (req, res, next) => {
+//     res.redirect('/login');
+// };

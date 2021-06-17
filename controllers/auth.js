e6f5f78d1a4e48ce3da3eed1360d582e7f4f9d4d
ruben-xe <rod17015@byui.edu>
2021-06-17 05:31:28 -0300

    signup route

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,12 +2,21 @@ const { validationResult } = require('express-validator/check');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
+const User = require('../models/user');
 
+exports.getSignup = (req, res, next) => {
+    res.render('auth/signup', {
+      pageTitle: 'Signup',
+      path: '/signup'
+    });
+  };
 exports.login = (req, res, next) => {
     const email = req.body.email;
   const password = req.body.password;
+//   console.log(email);
+
   let loadedUser;
-  User.findOne({ email: email })
+  User.findOne({ userEmail: email })
     .then(user => {
       if (!user) {
         const error = new Error('A user with this email could not be found.');
@@ -15,7 +24,7 @@ exports.login = (req, res, next) => {
         throw error;
       }
       loadedUser = user;
-      return bcrypt.compare(password, user.password);
+      return bcrypt.compare(password, user.userPassword);
     })
     .then(isEqual => {
       if (!isEqual) {
@@ -25,7 +34,7 @@ exports.login = (req, res, next) => {
       }
       const token = jwt.sign(
         {
-          email: loadedUser.email,
+          email: loadedUser.userEmail,
           userId: loadedUser._id.toString()
         },
         'supersecretsecret',
@@ -43,28 +52,32 @@ exports.login = (req, res, next) => {
 };
 
 exports.signup = (req, res, next) => {
-    const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    const error = new Error('Validation failed.');
-    error.statusCode = 422;
-    error.data = errors.array();
-    throw error;
-  }
+//     const errors = validationResult(req);
+//   if (!errors.isEmpty()) {
+//     const error = new Error('Validation failed.');
+//     error.statusCode = 422;
+//     error.data = errors.array();
+//     throw error;
+//   }
   const email = req.body.email;
   const name = req.body.name;
+  const lastName = req.body.lastName;
   const password = req.body.password;
+  console.log(email, name, lastName, password)
   bcrypt
     .hash(password, 12)
     .then(hashedPw => {
       const user = new User({
-        email: email,
-        password: hashedPw,
-        name: name
+        userEmail: email,
+        userPassword: hashedPw,
+        userName: name,
+        userLastName: lastName
       });
       return user.save();
     })
     .then(result => {
       res.status(201).json({ message: 'User created!', userId: result._id });
+      res.redirect('/');
     })
     .catch(err => {
       if (!err.statusCode) {

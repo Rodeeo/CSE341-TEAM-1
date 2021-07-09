392f4260ffaad1b45694074d4ffc736f1f7f3786
saraytin <smee.tindall@gmail.com>
2021-07-09 16:35:54 -0500

    very basic fix to unsearched total time

--- controllers/time.js
+++ controllers/time.js
@@ -66,9 +66,9 @@ exports.getTotal = (req, res, next) => {
   const page = +req.query.page || 1;
   const date1 = req.query.startDate;
   const date2 = req.query.endDate;
-  let totalItems=0
+  let totalItems = 0
   let totalHours = 0;
-  let data=[];
+  let data = [];
   if (date1 != undefined && date2 != undefined) {
     Time.find({
       userId: req.user._id
@@ -104,20 +104,43 @@ exports.getTotal = (req, res, next) => {
         lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
       });
     })
-  }else{
-    res.render('time/total', {
-      pageTitle: 'Total Time',
-      path: '/total',
-      data: data,
-      date1: date1,
-      date2: date2,
-      totalHours: totalHours,
-      currentPage: page,
-      hasNextPage: ITEMS_PER_PAGE * page < totalItems,
-      hasPreviousPage: page > 1,
-      nextPage: page + 1,
-      previousPage: page - 1,
-      lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
-    });
+  } else {
+    // TODO: fix pagination (unsure why it breaks after changing pages)
+    // TODO: fix or at least simplify non-searched results
+    // This solution is both redundant and crude
+    Time.find({
+      userId: req.user._id
+    }).
+    then(times => {
+      times = times.filter(e => {
+        let d1 = new Date('January 1, 2000 00:00:00');
+        let d2 = new Date('January 1, 2100 00:00:00');
+        let st = new Date(e.date);
+        if (st >= d1 && st <= d2) {
+          return true;
+        } else {
+          return false;
+        }
+      });
+      times.forEach(a => {
+        totalHours = totalHours + (+a.duration);
+      });
+      totalItems = times.length;
+      data = times.slice((page - 1) * ITEMS_PER_PAGE, ((page - 1) * ITEMS_PER_PAGE) + ITEMS_PER_PAGE);
+      res.render('time/total', {
+        pageTitle: 'Total Time',
+        path: '/total',
+        data: data,
+        date1: date1,
+        date2: date2,
+        totalHours: totalHours,
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE)
+      });
+    })
   }
 };
